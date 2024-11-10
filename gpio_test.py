edabd6f8fa371b1de1542858b08303f3a80e2dff
chibaf <chibaf@mac.com>
2024-11-10 23:59:14 +0900

    Add files via upload

--- gpio_test.py
+++ gpio_test.py
@@ -4,47 +4,21 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 # 11,12,13,15,16,18 = GPIO pin No. on Raspberry Pi board
-GPIO.setup(11,GPIO.OUT)
-GPIO.setup(12,GPIO.OUT)
-GPIO.setup(13,GPIO.OUT)
-GPIO.setup(15,GPIO.OUT)
-GPIO.setup(16,GPIO.OUT)
-GPIO.setup(18,GPIO.OUT)
-#
-GPIO.output(11, 0)
-GPIO.output(12, 0)
-GPIO.output(13, 0)
-GPIO.output(15, 0)
-GPIO.output(16, 0)
-GPIO.output(18, 0)
+gpin=[11,12,13,15,16,18]
+for i in range(0,len(gpin)):
+  GPIO.setup(gpin[i],GPIO.OUT)
+for i in range(0,len(gpin)):
+  GPIO.output(gpin[i], 0)
 #
 while True:
   try:
-    GPIO.output(11, 1)
-    time.sleep(0.5)
-    GPIO.output(12, 1)
-    time.sleep(0.5)
-    GPIO.output(13, 1)
-    time.sleep(0.5)
-    GPIO.output(15, 1)
-    time.sleep(0.5)
-    GPIO.output(16, 1)
-    time.sleep(0.5)
-    GPIO.output(18, 1)
-    time.sleep(0.5)
+    for i in range(0,len(gpin)):
+      GPIO.output(gpin[i], 1)
+      time.sleep(0.5)
 #
-    GPIO.output(11, 0)
-    time.sleep(0.5)
-    GPIO.output(12, 0)
-    time.sleep(0.5)
-    GPIO.output(13, 0)
-    time.sleep(0.5)
-    GPIO.output(15, 0)
-    time.sleep(0.5)
-    GPIO.output(16, 0)
-    time.sleep(0.5)
-    GPIO.output(18, 0)
-    time.sleep(0.5)
+    for i in range(0,len(gpin)):
+      GPIO.output(gpin[i], 0)
+      time.sleep(0.5)
   except KeyboardInterrupt:
     print("KeyboardInterrupt")
     GPIO.cleanup()
