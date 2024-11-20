ed352e3cd1b202cdd6290293d021630a17c18275
chibaf <chibaf@mac.com>
2024-11-21 02:59:53 +0900

    Add files via upload

--- ssr_th_main4.py
+++ ssr_th_main4.py
@@ -26,7 +26,6 @@ while True:
     elif threading.active_count()<len(gpio)+1:
       for i in range(0,len(gpio)):
         if qu[i].get()==0:
-          qu[i].put(1)
           t_on=random.randrange(2,5,1)
           t_off=random.randrange(1,5,1)
           th[i]=threading.Thread(target=ssrs[i].run,args=(t_on,t_off,qu[i]),daemon=True)
